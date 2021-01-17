857da52ca3564e7e8e547c9935d488672764325c
andrey <admiralleopard@gmail.com>
2021-01-17 19:34:30 +0700

    pages, router add

--- assets/components/pages/ProjectWorkspace.js
+++ assets/components/pages/ProjectWorkspace.js
@@ -3,7 +3,7 @@ import React from 'react';
 class ProjectWorkspace extends React.Component {
     render() {
         return (
-
+            <div className={"project-workspace"}></div>
         );
     }
 }
