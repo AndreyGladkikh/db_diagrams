f92d7eece04b8cbaa22fd0cd815d305465161443
andrey <admiralleopard@gmail.com>
2021-01-17 12:35:36 +0700

    docker fix, flusher

--- src/Model/Flusher.php
+++ src/Model/Flusher.php
@@ -4,10 +4,22 @@
 namespace App\Model;
 
 
+use Doctrine\ORM\EntityManagerInterface;
+
 class Flusher
 {
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     public function flush()
     {
-
+        $this->em->flush();
     }
 }
